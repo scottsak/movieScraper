39da659ccd24f29590b8d84c47fd8e71cb71a66e
scottsak <48599109+scottsak@users.noreply.github.com>
2022-08-17 23:05:26 -0700

    gets the general info of the page

--- movieScraper.py
+++ movieScraper.py
@@ -1,7 +1,16 @@
 from bs4 import BeautifulSoup
 from pip._vendor import requests
 
-url = "https://www.themoviedb.org/movie/696806"
+#adam project 16
+# url = "https://www.themoviedb.org/movie/696806" 
+
+#top gun maverick 17
+# url = "https://www.themoviedb.org/movie/361743"
+
+#sorcerers 29
+url = "https://www.themoviedb.org/movie/27022-the-sorcerer-s-apprentice"
+
+#first movie in db
 # url = "https://www.themoviedb.org/movie/1"
 
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36'}
@@ -12,6 +21,10 @@ doc = BeautifulSoup(result.text, "html.parser")
 
 mydivs = doc.find_all("div", {"class": "single_column"})
 
-tag = doc.body
+generalInfo = doc.find_all("div", {"class": "ott_true"})
 
-print(mydivs)
+# tag = doc.body
+
+# print(mydivs)
+
+print(generalInfo)
