4c3b49c517564d67a9a5df7dc532aa2a82163293
scottsak <48599109+scottsak@users.noreply.github.com>
2022-08-19 02:16:37 -0700

    cleaned up the code

--- movieScraper.py
+++ movieScraper.py
@@ -2,12 +2,7 @@ from bs4 import BeautifulSoup
 from pip._vendor import requests
 import mysql.connector
 
-
-baseURL = "https://www.themoviedb.org/movie/"
-
-movieNum = str(550)
-
-url = baseURL + movieNum
+# different movies for testing
 
 #fight club
 # url = "https://www.themoviedb.org/movie/550"
@@ -24,41 +19,44 @@ url = baseURL + movieNum
 #first movie in db
 # url = "https://www.themoviedb.org/movie/1"
 
+
+#url setup
+baseURL = "https://www.themoviedb.org/movie/"
+
+movieNum = str(550)
+
+url = baseURL + movieNum
+
+#getting header
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36'}
 
+#parsing doc for general info
 result = requests.get(url, headers=headers)
 
 doc = BeautifulSoup(result.text, "html.parser")
 
-# print(doc)
-
 generalInfo = doc.find_all("div", {"class": "title"})
 
-
-voteURLBase = "https://www.themoviedb.org/movie/"+movieNum+"/remote/rating/details?translate=false&language=en-US&locale=en-US"
-
-voteResult = requests.get(voteURLBase, headers=headers)
-
-doc = BeautifulSoup(voteResult.text, "html.parser")
-
-rating = doc.find_all("div", {"class":"section"})[0].find("h3").string.split(" ")[0]
-
-print(rating)
-
-# voteCount = str(doc.find_all("script")[12])
-
-# print(voteCount)
-
 if len(generalInfo) != 0:
+
+    # gets the title, id, and date 
     title = generalInfo[0].find('a').string
-
     id = str(generalInfo[0].find_all(href=True)[0]).split('/')[2].split('-')[0]
-
     date = generalInfo[0].find_all("span", {"class" : "release_date"})[0].string
 
+    # prints to test
     print(title)
     print(id)
     print(date)
+
+    # gets info for the vote count 
+    voteURLBase = "https://www.themoviedb.org/movie/"+movieNum+"/remote/rating/details?translate=false&language=en-US&locale=en-US"
+    voteResult = requests.get(voteURLBase, headers=headers)
+    doc = BeautifulSoup(voteResult.text, "html.parser")
+    rating = doc.find_all("div", {"class":"section"})[0].find("h3").string.split(" ")[0]
+
+    # tests rating
+    print(rating)
 
 
     # db = mysql.connector.connect(
